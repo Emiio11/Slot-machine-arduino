6b3b68dfdfae73a9dd6b6acae3e8aebdaa37b55c
Emilio Tulli <emiliotulli03@gmail.com>
2023-12-05 10:30:20 +0100

    readme

---
+++ index.js
@@ -0,0 +1,30 @@
+// index.js
+const Mustache = require('mustache');
+const fs = require('fs');
+const MUSTACHE_MAIN_DIR = './main.mustache';
+
+let DATA = {
+  name: 'Emilio',
+  date: new Date().toLocaleDateString('it-IT', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    timeZoneName: 'short',
+    timeZone: 'Europe/Rome',
+  }),
+};
+/**
+  * A - We open 'main.mustache'
+  * B - We ask Mustache to render our file with the data
+  * C - We create a README.md file with the generated output
+  */
+function generateReadMe() {
+  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
+    if (err) throw err;
+    const output = Mustache.render(data.toString(), DATA);
+    fs.writeFileSync('README.md', output);
+  });
+}
+generateReadMe();
